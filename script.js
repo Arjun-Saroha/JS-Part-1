90305551eb846e8b2943b3506dca173fa2136013
Arjun Saroha <104119028+Arjun-Saroha@users.noreply.github.com>
2025-02-07 21:43:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,141 @@
+// ADD Two Numbers
+// const num1 = 5;
+// const num2 = 3;
+// const sum = num1 + num2;
+// console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum); //The sum of 5 and 3 is: 8
+
+// ......................................................................................................................................
+
+
+//Find if the conditions are obeyed or not?
+// let x = 10;
+// let y = 20;
+// if (x < y) {
+//   console.log("x is less than y");
+// } else if (x > y) {
+//   console.log("x is greater than y");
+// } else {
+//   console.log("x is equal to y");//x is less than y
+// }
+
+//..............................................................................................................................................
+
+// Check the conditions
+// You are given two numbers A? and B? . You need to do the following checks:
+// 1. if both are divisible by 10 console true.
+// 2. if both are not divisible by 10 console false.
+// 3. if one of them only is divisible by 10 console true
+
+// let A = 20, B = 50; 
+// console.log((A % 10 === 0) || (B % 10 === 0));//true
+
+
+
+
+  //.............................................................................................................................................
+
+//   Find the first digit of a 4 digit number
+// You are provided a four digit number N only. Your task is to print out the first digit of that
+// number.
+// Note: You have to complete First_Digit function. No need to take any input.
+
+// let N = 1234; 
+
+// let firstDigit = Math.floor(N / 1000);
+
+// console.log(firstDigit);//1
+
+
+
+
+// ..............................................................................................................................................
+
+// Find the last digit of a 4 digit number
+// You are provided a four digit number N only. Your task is to print out the last digit of that
+// number.
+// Note: You have to complete Last_Digit function. No need to take any input.
+
+// let N = 1234; 
+
+// let lastDigit = N % 10;
+
+// console.log(lastDigit);//4
+
+
+
+
+
+
+
+
+
+//..............................................................................................................................................
+
+
+// Find the remainder
+// You are provided with two numbers A?and B?where A?as divisor and B?as dividend.Your
+// task is find the remainder.
+// Note: You have to complete Find_the_remainder function. No need to take any input.
+
+
+// let A = 5; 
+// let B = 12; 
+
+// let remainder = B % A;
+
+// console.log(remainder);//2
+
+
+
+
+
+
+
+
+
+
+//.............................................................................................................................................
+
+
+
+
+// Multiply two Numbers
+// You are provided with two numbers A?and B?. Your task is to multiply these two numbers.
+// Note: You have to complete Multiply_two_number function. No need to take any input.
+
+// let A = 5;
+// let B = 10; 
+
+// let result = A * B;
+
+// console.log(result);//50
+
+
+//..............................................................................................................................................
+
+
+
+
+// Marks Calculator
+// Shyam has got his marks in three subjects as A?, B?, and C?(out of 100).Write a program to
+// calculate his total marks and his average.
+// Note: You have to complete Sum and Average functions. No need to take any input.
+
+
+let A = 85; 
+let B = 90; 
+let C = 80; 
+
+
+let total = A + B + C;
+
+
+let average = total / 3;
+
+
+console.log("Total Marks:", total);
+console.log("Average Marks:", average);
+                                            //  Total Marks: 255
+                                            //  Average Marks: 85
+
+
